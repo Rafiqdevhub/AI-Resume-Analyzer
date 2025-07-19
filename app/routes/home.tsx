087f805eb5e6f677b36be6bcdabfdf4915798726
MUhammad Rafiq <rafiq85825@gmail.com>
2025-07-19 05:36:42 +0500

    Navbar added

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,25 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "AI Resume Analyzer" },
+    { name: "description", content: "Analyze your resume with AI" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Analyze Your Resume</h1>
+          <h2>
+            Get insights and improve your chances of landing your dream job
+          </h2>
+        </div>
+      </section>
+    </main>
+  );
 }
